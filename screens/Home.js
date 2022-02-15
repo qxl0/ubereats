e758773fd993d7817a1b769162b5261fb7115373
qiang li <qxl0@yahoo.com>
2022-02-14 19:15:57 -0500

    Set up HeaderTabs, ... Structure

---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+import React from "react";
+import HeaderTabs from "../components/HeaderTabs";
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <HeaderTabs />
+    </SafeAreaView>
+  );
+}
