34721ed29fbffbafa57a1e529180e3a551ac741d
qiang li <qxl0@yahoo.com>
2022-02-14 19:31:33 -0500

    Added Styles to the HeaderTab/buttons

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,10 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 
 export default function HeaderTabs() {
   return (
-    <View>
-      <Text>HeaderTabs</Text>
+    <View style={{ flexDirection: "row", alignSelf: "center" }}>
+      <HeaderButton title="Delivery" />
+      <HeaderButton title="Pickup" />
     </View>
   );
 }
+
+const HeaderButton = ({ title }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        backgroundColor: "black",
+        paddingVertical: 6,
+        paddingHorizontal: 16,
+      }}
+    >
+      <Text style={{ color: "white" }}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
