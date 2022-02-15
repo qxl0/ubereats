884fab950e9e69fa51efe5a33a068da26f7fdb10
qiang li <qxl0@yahoo.com>
2022-02-15 08:56:12 -0500

    Styling googleplacessearch

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -5,7 +5,24 @@ import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplet
 export default function SearchBar() {
   return (
     <View style={{ marginTop: 15, flexDirection: "row" }}>
-      <GooglePlacesAutocomplete placeholder="Search" />
+      <GooglePlacesAutocomplete
+        placeholder="Search"
+        styles={{
+          textInput: {
+            backgroundColor: "#eee",
+            borderRadius: 20,
+            fontWeight: "700",
+            marginTop: 7,
+          },
+          textInputContainer: {
+            backgroundColor: "#eee",
+            borderRadius: 50,
+            flexDirection: "row",
+            alignItems: "center",
+            marginRight: 10,
+          },
+        }}
+      />
     </View>
   );
 }
