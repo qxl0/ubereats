62a7afae65d4d767e5fb20d7a0c487775076d89c
qiang li <qxl0@yahoo.com>
2022-02-14 20:35:10 -0500

    Start working on Google SearchBar

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,14 @@
 import { SafeAreaView, Text, View } from "react-native";
 import React from "react";
 import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
         <HeaderTabs />
+        <SearchBar />
       </View>
     </SafeAreaView>
   );
