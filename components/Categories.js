c9b746fe477176143d8c15999f79f426a8f6cf8f
qiang li <qxl0@yahoo.com>
2022-02-15 19:46:58 -0500

    code categories

--- components/Categories.js
+++ components/Categories.js
@@ -1,10 +1,55 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 
+const items = [
+  {
+    image: require("../assets/images/shopping-bag.png"),
+    title: "Pick-up",
+  },
+  {
+    image: require("../assets/images/soft-drink.png"),
+    title: "Soft Drinks",
+  },
+  {
+    image: require("../assets/images/bread.png"),
+    title: "Bakery Items",
+  },
+  {
+    image: require("../assets/images/fast-food.png"),
+    title: "Fast Foods",
+  },
+  {
+    image: require("../assets/images/deals.png"),
+    title: "Deals",
+  },
+  {
+    image: require("../assets/images/coffee.png"),
+    title: "Coffe & Tea",
+  },
+  {
+    image: require("../assets/images/desserts.png"),
+    title: "Desserts",
+  },
+];
 export default function Categories() {
   return (
     <View>
-      <Text>Categories</Text>
+      <Image
+        source={items[0].image}
+        style={{
+          width: 50,
+          height: 40,
+          ersizeMode: "contain",
+        }}
+      />
+      <Text
+        style={{
+          fontSize: 13,
+          fontWeight: "900",
+        }}
+      >
+        PIck-up
+      </Text>
     </View>
   );
 }
