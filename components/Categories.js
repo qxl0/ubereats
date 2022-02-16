3a19cd052eb672037ea5d4d6c907eeb435b2448f
qiang li <qxl0@yahoo.com>
2022-02-15 19:54:07 -0500

    Changed to loops in Category

--- components/Categories.js
+++ components/Categories.js
@@ -1,4 +1,4 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 import React from "react";
 
 const items = [
@@ -33,23 +33,27 @@ const items = [
 ];
 export default function Categories() {
   return (
-    <View>
-      <Image
-        source={items[0].image}
-        style={{
-          width: 50,
-          height: 40,
-          ersizeMode: "contain",
-        }}
-      />
-      <Text
-        style={{
-          fontSize: 13,
-          fontWeight: "900",
-        }}
-      >
-        PIck-up
-      </Text>
-    </View>
+    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+      {items.map((item, index) => (
+        <View key={index} style={{ alignItems: "center", marginRight: 30 }}>
+          <Image
+            source={item.image}
+            style={{
+              width: 50,
+              height: 40,
+              ersizeMode: "contain",
+            }}
+          />
+          <Text
+            style={{
+              fontSize: 13,
+              fontWeight: "900",
+            }}
+          >
+            {item.title}
+          </Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
