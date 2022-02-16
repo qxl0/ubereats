3cc8d4cb4cb6d758ea4a97dad486e0edb598a8b0
qiang li <qxl0@yahoo.com>
2022-02-15 20:17:44 -0500

    Building RestaurantItem

--- components/RestaurantItem.js
+++ components/RestaurantItem.js
@@ -1,10 +1,35 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function RestaurantItem() {
   return (
     <View>
-      <Text>RestaurantItem</Text>
+      {/* Image */}
+      <RestaurantImage />
+      {/* info */}
     </View>
   );
 }
+
+const RestaurantImage = () => {
+  return (
+    <>
+      <Image
+        style={{ width: "100%", height: 180 }}
+        source={{
+          uri: "https://c4.wallpaperflare.com/wallpaper/741/599/723/pizza-food-vegetables-fruit-wallpaper-preview.jpg",
+        }}
+      />
+      <TouchableOpacity
+        style={{
+          position: "absolute",
+          right: 20,
+          top: 20,
+        }}
+      >
+        <MaterialCommunityIcons name="heart-outline" size={30} color="red" />
+      </TouchableOpacity>
+    </>
+  );
+};
