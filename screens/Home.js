c6c18949d673210b83df68dbf7df6229a685653b
qiang li <qxl0@yahoo.com>
2022-02-16 19:33:31 -0500

    Yelp Api works

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import { SafeAreaView, ScrollView, Text, View } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
 import Categories from "../components/Categories";
@@ -7,8 +7,33 @@ import RestaurantItems, {
   localRestaurants,
 } from "../components/RestaurantItems";
 
+const YELP_API_KEY =
+  "p3_HRi88-pD970fEe7QHFAn5wR4E4lSmwABT5L-U6rCWkaiP7W7ti4ohp_In2ACB_uq1nWMayrtyjdCbi0t_FK2fraE-_EKl9-zVNBt8mEYu4q6jTaLhSgqcu5UNYnYx";
+
 export default function Home() {
   const [restaurantData, setRestaurantData] = useState(localRestaurants);
+
+  const getRestaurants = async () => {
+    const yelpurl =
+      "https://api.yelp.com/v3/businesses/search?term=restaurants&location=philadelphia&limit=50";
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return fetch(yelpurl, apiOptions)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log("json", json);
+        setRestaurantData(json.businesses);
+      });
+  };
+
+  useEffect(() => {
+    getRestaurants();
+  }, []);
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
