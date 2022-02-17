e1b9d355836f43939e39cafb972167b5bf63aba1
qiang li <qxl0@yahoo.com>
2022-02-16 21:09:18 -0500

    Added HomeTabs Icon

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -1,10 +1,33 @@
 import { View, Text } from "react-native";
 import React from "react";
-
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 export default function BottomTabs() {
   return (
-    <View>
+    <View
+      style={{
+        flexDirection: "row",
+        margin: 10,
+        marginHorizontal: 30,
+        justifyContent: "space-between",
+      }}
+    >
+      <Icon icon="home" text="Home" />
+      <Text>BottomTabs</Text>
       <Text>BottomTabs</Text>
     </View>
   );
 }
+
+const Icon = (props) => (
+  <View>
+    <FontAwesome5
+      name={props.icon}
+      icon={25}
+      style={{
+        marginBottom: 3,
+        alignSelf: "center",
+      }}
+    />
+    <Text>{props.text}</Text>
+  </View>
+);
