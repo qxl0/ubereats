0e22940881a90dc529b77156e1ebb78e0e68567c
qiang li <qxl0@yahoo.com>
2022-02-17 19:29:26 -0500

    Restaurant Detail

--- components/home/restaurantDetail/About.js
+++ components/home/restaurantDetail/About.js
@@ -1,4 +1,4 @@
-import { View, Image } from "react-native";
+import { View, Image, Text } from "react-native";
 import React from "react";
 
 const image =
@@ -10,10 +10,38 @@ export default function About() {
   return (
     <View>
       <RestaurantImage image={image} />
+      <RestaurantTitle title={title} />
+      <RestaurantDescription description={description} />
     </View>
   );
 }
 
 const RestaurantImage = (props) => (
   <Image source={{ uri: props.image }} style={{ width: "100%", height: 180 }} />
+);
+
+const RestaurantTitle = (props) => (
+  <Text
+    style={{
+      fontSize: 29,
+      fontWeight: "600",
+      marginTop: 10,
+      marginHorizontal: 15,
+    }}
+  >
+    {props.title}
+  </Text>
+);
+
+const RestaurantDescription = (props) => (
+  <Text
+    style={{
+      marginTop: 10,
+      marginHorizontal: 15,
+      fontWeight: "400",
+      fontSize: 15.5,
+    }}
+  >
+    {props.description}
+  </Text>
 );
