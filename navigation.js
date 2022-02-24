316fda170d10df41181e36a2dda8e8db391a2907
qiang li <qxl0@yahoo.com>
2022-02-24 11:50:47 -0500

    Added ReduxProvider to the app root to offer Redux support

--- navigation.js
+++ navigation.js
@@ -3,6 +3,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import Home from "./screens/Home";
 import RestaurantDetail from "./screens/RestaurantDetail";
+import { Provider as ReduxProvider } from "react-redux";
+import store from "./redux/store";
 
 export default function RootNavigation() {
   const stack = createStackNavigator();
@@ -12,11 +14,13 @@ export default function RootNavigation() {
   };
 
   return (
-    <NavigationContainer>
-      <stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
-        <stack.Screen name="Home" component={Home} />
-        <stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
-      </stack.Navigator>
-    </NavigationContainer>
+    <ReduxProvider store={store}>
+      <NavigationContainer>
+        <stack.Navigator initialRouteName="Home" screenOptions={screenOptions}>
+          <stack.Screen name="Home" component={Home} />
+          <stack.Screen name="RestaurantDetail" component={RestaurantDetail} />
+        </stack.Navigator>
+      </NavigationContainer>
+    </ReduxProvider>
   );
 }
