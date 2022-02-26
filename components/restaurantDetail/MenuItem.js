3a28dcc77163de778ce7b9e55539bcadf7be9f12
qiang li <qxl0@yahoo.com>
2022-02-25 20:32:54 -0500

    Refactor menuItem

--- components/restaurantDetail/MenuItem.js
+++ components/restaurantDetail/MenuItem.js
@@ -5,43 +5,12 @@ import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 
-const foodsList = [
-  {
-    title: "Lasagna",
-    description: "A delicious pasta dish",
-    price: "$13.50",
-    image:
-      "https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408?k=20&m=1155240408&s=612x612&w=0&h=Zvr3TwVQ-wlfBnvGrgJCtv-_P_LUcIK301rCygnirbk=",
-  },
-  {
-    title: "Tandoori Chicken",
-    description: "Amazing Indian dish with tenderloin chicken off the sizzle",
-    price: "$12.99",
-    image: "https://i.ytimg.com/vi/BKxGodX9NGg/maxresdefault.jpg",
-  },
-  {
-    title: "Chilaquiles",
-    description: "A delicious pasta dish",
-    price: "$19.50",
-    image:
-      "https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408?k=20&m=1155240408&s=612x612&w=0&h=Zvr3TwVQ-wlfBnvGrgJCtv-_P_LUcIK301rCygnirbk=",
-  },
-  {
-    title: "Chicken Caesar Salad",
-    description: "Amazing Indian dish with tenderloin chicken off the sizzle",
-    price: "$18.99",
-    image: "https://i.ytimg.com/vi/BKxGodX9NGg/maxresdefault.jpg",
-  },
-  {
-    title: "Lasagna 3",
-    description: "A delicious pasta dish",
-    price: "$23.50",
-    image:
-      "https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408?k=20&m=1155240408&s=612x612&w=0&h=Zvr3TwVQ-wlfBnvGrgJCtv-_P_LUcIK301rCygnirbk=",
-  },
-];
-
-export default function MenuItem({ restaurantName }) {
+export default function MenuItem({
+  restaurantName,
+  foods,
+  hideCheckbox,
+  marginLeft,
+}) {
   const dispatch = useDispatch();
   const items = useSelector((state) => state.cartReducer.selectedItems.items);
 
@@ -65,20 +34,24 @@ export default function MenuItem({ restaurantName }) {
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-      {foodsList.map((food, index) => (
+      {foods.map((food, index) => (
         <View key={index}>
           <View style={styles.menuItemStyle}>
-            <BouncyCheckbox
-              iconStyle={{
-                borderColor: "lightgray",
-                borderRadius: 0,
-              }}
-              onPress={(checkboxValue) => selectItem(food, checkboxValue)}
-              isChecked={isSelected(food, items)}
-              fillColor="green"
-            />
+            {hideCheckbox ? (
+              <></>
+            ) : (
+              <BouncyCheckbox
+                iconStyle={{
+                  borderColor: "lightgray",
+                  borderRadius: 0,
+                }}
+                onPress={(checkboxValue) => selectItem(food, checkboxValue)}
+                isChecked={isSelected(food, items)}
+                fillColor="green"
+              />
+            )}
             <FoodInfo food={food} />
-            <FoodImage food={food} />
+            <FoodImage food={food} marginLeft={marginLeft ? marginLeft : 0} />
           </View>
           <Divider
             width={0.5}
@@ -99,11 +72,16 @@ const FoodInfo = ({ food }) => (
   </View>
 );
 
-const FoodImage = ({ food }) => (
+const FoodImage = ({ marginLeft, ...food }) => (
   <View>
     <Image
       source={{ uri: food.image }}
-      style={{ width: 80, height: 80, borderRadius: 20 }}
+      style={{
+        width: 80,
+        height: 80,
+        borderRadius: 20,
+        marginLeft: marginLeft,
+      }}
     />
   </View>
 );
--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -5,12 +5,47 @@ import About from "../components/restaurantDetail/About";
 import MenuItem from "../components/restaurantDetail/MenuItem";
 import ViewCart from "../components/restaurantDetail/ViewCart";
 
+const foods = [
+  {
+    title: "Lasagna",
+    description: "A delicious pasta dish",
+    price: "$13.50",
+    image:
+      "https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408?k=20&m=1155240408&s=612x612&w=0&h=Zvr3TwVQ-wlfBnvGrgJCtv-_P_LUcIK301rCygnirbk=",
+  },
+  {
+    title: "Tandoori Chicken",
+    description: "Amazing Indian dish with tenderloin chicken off the sizzle",
+    price: "$12.99",
+    image: "https://i.ytimg.com/vi/BKxGodX9NGg/maxresdefault.jpg",
+  },
+  {
+    title: "Chilaquiles",
+    description: "A delicious pasta dish",
+    price: "$19.50",
+    image:
+      "https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408?k=20&m=1155240408&s=612x612&w=0&h=Zvr3TwVQ-wlfBnvGrgJCtv-_P_LUcIK301rCygnirbk=",
+  },
+  {
+    title: "Chicken Caesar Salad",
+    description: "Amazing Indian dish with tenderloin chicken off the sizzle",
+    price: "$18.99",
+    image: "https://i.ytimg.com/vi/BKxGodX9NGg/maxresdefault.jpg",
+  },
+  {
+    title: "Lasagna 3",
+    description: "A delicious pasta dish",
+    price: "$23.50",
+    image:
+      "https://media.istockphoto.com/photos/food-backgrounds-table-filled-with-large-variety-of-food-picture-id1155240408?k=20&m=1155240408&s=612x612&w=0&h=Zvr3TwVQ-wlfBnvGrgJCtv-_P_LUcIK301rCygnirbk=",
+  },
+];
 export default function RestaurantDetail({ route, navigation }) {
   return (
     <View>
       <About route={route} />
       <Divider style={{ marginVertical: 20 }} />
-      <MenuItem restaurantName={route.params.name} />
+      <MenuItem restaurantName={route.params.name} foods={foods} />
       <ViewCart navigation={navigation} restaurantName={route.params.name} />
     </View>
   );
